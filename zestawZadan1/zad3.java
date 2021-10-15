717eb41f689a407e14a5faff4ae24f53d2b1b016
KrzycheG <kpstrowski@gmail.com>
2021-10-15 17:39:31 +0200

    Zadanie 3 koniec

---
+++ zestawZadan1/zad3.java
@@ -0,0 +1,33 @@
+package zestawZadan1;
+
+public class zad3 {
+
+
+    public static String simplify(String str) {
+
+        char[] table = str.toCharArray();
+        char[] table1 = new char[str.length()];
+        table1[0]= table[0];
+        int j = 1;
+
+        for (int i = 0; i < table.length-1; i++) {
+
+            if(table[i] != table[i+1]){
+
+                table1[j] = table[i+1];
+                j++;
+            }
+        }
+
+        String result = new String(table1);
+        String subString = result.substring(0,result.indexOf('\0'));
+
+        return subString;
+
+    }
+
+    public static void main(String args[]){
+
+        System.out.println(simplify("aaabbbbbbbaaaccd"));
+    }
+ }
