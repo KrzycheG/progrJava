a1d1ec955eed6feb03321b9d074934181a47c177
KrzycheG <kpstrowski@gmail.com>
2021-10-18 12:59:32 +0200

    Zadanie 5 poprawione

--- zestawZadan1/zad5.java
+++ zestawZadan1/zad5.java
@@ -1,50 +1,73 @@
 package zestawZadan1;
 
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class zad5 {
 
 
     public static String cesarCipher(String str, int key){
 
-        // W odróznieniu od tradycyjnego szyfru cezara, metoda ta działa na znakach od
-        // Dec 33-126 z Tablicy kodu ASCII i przesuwając litery do tyłu.
+        /*
+        W odróznieniu od tradycyjnego szyfru cezara, metoda ta działa na znakach od
+        Dec 33-126 z Tablicy kodu ASCII przesuwając znaki w tył.
+        */
 
         byte key1 =(byte) key;
         char[] table = str.toCharArray();
 
         for(int i = 0; i<str.length();i++){
 
-            byte Dif = (byte) (table[i]-key1);
+            if(table[i] != ' ') {
 
-            if(Dif < 33){                 // Zejście poniżej ustalonego zakresu 33- 126
+                byte Dif = (byte) (table[i] - key1);
 
-                if( key1 > 33 ){          // tu cos nie trybi
+                if (Dif < 33) {                         /// Wyjscie przesunięcia poza zakres dopuszczalnych liczb.
 
-                    table[i] = (char) (126 -(Math.abs(Dif)+33));
+                    if (Dif < 0) {
+
+                        table[i] = (char) (127 - ((Math.abs(Dif)) + 33));
+
+                    } else {
+
+                        table[i] = (char) (127 - (33 - Dif));
+
+                    }
+                } else {
+
+                    table[i] = (char) (Dif);
 
                 }
-                else{
-
-                    table[i] = (char) (126- ((Dif)- 33));
-
-                }
-
-
-            }
-            else{
-
-                table[i] = (char)(Dif); ;
 
             }
         }
-        String result = new String(table);
 
-            return result;
+        return new String(table);
     }
     public static void main(String[] Args){
 
-        System.out.println(cesarCipher("AprytnyCezar", 40));
+        Scanner scan = new Scanner(System.in);
+
+        String sentence ;
+        int number ;
+        System.out.println("Podaj zdanie które chcesz zaszyfrować");
+        sentence = scan.nextLine();
+        while(true){
+            System.out.println("Podaj liczbe z zakresu 1-127:");
+            number = scan.nextInt();
+            if(number >=1 && number <=127 ){
+                break;
+            }
+            else{
+                System.out.println("Podano liczbe z poza zakresu, proszę spróbój ponownie:");
+            }
+        }
+
+
+
+        System.out.println(cesarCipher(sentence, number));
 
     }
 }
