8595f1b73bab64c8a5a5e4d844b7a5b948cff3b5
KrzycheG <kpstrowski@gmail.com>
2021-10-26 19:42:07 +0200

    Zad 2 Zestaw 2

--- zestawZadan2/zad2.java
+++ zestawZadan2/zad2.java
@@ -1,4 +1,37 @@
 package zestawZadan2;
 
+import java.util.Random;
+
 public class zad2 {
+
+    /* Uzytkownik podaje długosc tablicy oraz element do policzenia.
+         
+     */
+
+    public static int randTab (int length, char ch){
+
+        Random random = new Random();
+        char[] tab = new char[length];
+        int count = 0;
+        for(int i = 0 ; i<length;i++){
+
+            tab[i] = (char) (random.nextInt(94) +32);
+            if(tab[i] == ch){
+                count ++;
+            }
+
+        }
+
+        for(char x:tab){
+            System.out.print(x);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args){
+
+        System.out.println("  Liczba wystąpien podanego znaku: " + (randTab(20,'4')));
+
+    }
 }
