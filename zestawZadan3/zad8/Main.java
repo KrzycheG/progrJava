366a0b3fcd6a4d9cc37139811842d5ce17adca63
KrzycheG <kpstrowski@gmail.com>
2021-11-27 10:40:26 +0100

    Zestaw 3 zad 8

--- zestawZadan3/zad8/Main.java
+++ zestawZadan3/zad8/Main.java
@@ -1,13 +1,63 @@
 package zestawZadan3.zad8;
 
 import java.io.EOFException;
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
 public class Main {
 
-    public static void main (String[] args) throws IOException, EOFException {
+    public static String housesDiscount(String fileName) throws IOException {
+
+        RandomAccessFile raf = new RandomAccessFile( fileName,"rw");
+
+        int maxFloor = 0;
+        String nazwDom = "";
+        int IKondygn;
+        double cena;
+        String str = "";
+
+        int pointer = 0;
+        while(pointer<raf.length()){
+
+            raf.seek(pointer);
+            if(raf.readByte() >= 33 && raf.readByte() <=126){
+                raf.seek(pointer);
+                char z = (char) raf.readByte();
+
+                str+= "" + z;
+                pointer++;
+
+            }
+            else {
+
+                raf.seek(pointer);
+                IKondygn = raf.readInt();
+                cena = raf.readDouble();
+                pointer += 12;
+
+                if(IKondygn == 1){
+
+                    raf.seek(pointer -8);
+                    raf.writeDouble(cena - (cena * 0.10));
+
+                }
+                if(IKondygn > maxFloor){
+
+                    maxFloor = IKondygn;
+                    nazwDom = str;
+
+                }
+
+                str= "";
+            }
+
+        }
+
+        return "House with the most floors: " + nazwDom;
+    }
+
+    public static void main (String[] args) throws IOException {
 
         RandomAccessFile raf = new RandomAccessFile( "file.txt","rw");
         raf.writeByte(65);
@@ -16,14 +66,13 @@ public class Main {
         raf.writeByte(65);
         raf.writeInt(1);
         raf.writeDouble(506.50);
-
         raf.writeByte(121);
         raf.writeByte(88);
         raf.writeByte(54);
-        raf.writeByte(23);
+        raf.writeByte(36);
+        raf.writeByte(56);
         raf.writeInt(5);
         raf.writeDouble(707.5);
-
         raf.writeByte(64);
         raf.writeByte(34);
         raf.writeByte(47);
@@ -31,50 +80,11 @@ public class Main {
         raf.writeInt(2);
         raf.writeDouble(505.5);
 
-
-        int pointer = 0;
-        int maxFloor = 0;
-        String finalString = "";
-        int in = 0;
-        double d = 0;
-        String str = "";
-
-        while(pointer<raf.length()){
-
-
-            raf.seek(pointer);
-            if(raf.readByte() >= 33 && raf.readByte() <=126){
-                raf.seek(pointer);
-                char z = (char) raf.readByte();
-                str+= "" + z;
-                pointer++;
-
-            }
-            else {
-                raf.seek(pointer);
-                in = raf.readInt();
-                d = raf.readDouble();
-                pointer += 12;
-
-                if(in == 1){
-
-                    raf.seek(pointer -8);
-                    raf.writeDouble(d - (d * 0.10));
-
-                }
-                if(in > maxFloor){
-
-                    maxFloor = in;
-                    finalString = str;
-
-                }
-                pointer += 12 +8;
-            }
-
-            System.out.println(str);
-            System.out.println(raf.length());
-        }
-
+        raf.seek(8);
+        System.out.println("Price of house with 1 floor: " + raf.readDouble());
+        System.out.println(housesDiscount("file.txt"));
+        raf.seek(8);
+        System.out.println("Price of house with 1 floor after 10% discount:  " + raf.readDouble());
 
 
 
