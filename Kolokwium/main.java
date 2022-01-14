08fd1fbf8e39473c6ee0dc76e102ce10db0316f6
pusio <kpstrowski@gmail.com>
2022-01-14 18:44:34 +0100

    asdasd

---
+++ Kolokwium/main.java
@@ -0,0 +1,12 @@
+package Kolokwium;
+
+public class main {
+
+    public static void main (String args[]){
+
+
+        System.out.println("adsadasd");
+        System.out.println("sdfdsfsd");
+
+    }
+}
