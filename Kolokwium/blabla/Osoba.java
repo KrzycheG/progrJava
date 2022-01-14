79e7649fab89f5bc6f45cab5d673eeb39a0376fe
pusio <kpstrowski@gmail.com>
2022-01-14 18:52:45 +0100

    asdasd

---
+++ Kolokwium/blabla/Osoba.java
@@ -0,0 +1,11 @@
+package Kolokwium.blabla;
+
+public class Osoba {
+    String name;
+    int wiek;
+
+    public Osoba(String name, int wiek) {
+        this.name = name;
+        this.wiek = wiek;
+    }
+}
